2293306f892eba07093067226daf5fd8325f82e7
DaniNajmi <dnajmiasl@gmail.com>
2025-03-05 13:49:26 -0600

    Added thingProperties library and defined variables

--- Lab7/Lab7.ino
+++ Lab7/Lab7.ino
@@ -1,9 +1,28 @@
+// BIOE 448 Lab 7
+// Daniela Najmias, Alice Zhou
+
+
+#include "thingProperties.h"
+int sensor_pin = 0;
+int pulse_signal = 0;
+int upper_threshold = 860; //or your threshold of choice
+int lower_threshold = 800; //or your threshold of choice
+int counter = 0;
+//float BPM = 0.0;
+bool ignore = false;
+bool first_pulse_detected = false;
+unsigned long first_pulse_time = 0;
+unsigned long second_pulse_time = 0;
+unsigned long pulse_period = 0;
+
+
+
 void setup() {
-  // put your setup code here, to run once:
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 
 }
